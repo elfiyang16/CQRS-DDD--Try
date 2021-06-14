f90f5530929e9e2c3f8b3ca5b301320d3f077661
Elfi_Y <elfiyang16@gmail.com>
2021-06-14 22:32:56 +0100

    feat: add queries

---
+++ bank_co/query.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+from uuid import UUID
+from bank_co.repository import BankRepo, CustomerRepo, AccountRepo
+from bank_co.entity import Customer, Bank, Aggregate, Account
+from bank_co.value_object import UniqueId
+
+
+class Query(ABC):
+    @abstractmethod
+    def execute(self) -> None:
+        pass
+
+
+class GetAccount(Query):
+    def __init__(self, account_id: UUID) -> None:
+        self._account_id = account_id
+
+    def execute(self) -> None:
+        return AccountRepo.find_by_id(UniqueId(self._account_id))
+
+
+class GetCustomer(Query):
+    def __init__(self, customer_id: UUID) -> None:
+        self._customer_id = customer_id
+
+    def execute(self) -> None:
+        return CustomerRepo.find_by_id(UniqueId(self._customer_id))
+
+
+class GetBank(Query):
+    def __init__(self, bank_id: UUID) -> None:
+        self._bank_id = bank_id
+
+    def execute(self) -> None:
+        return BankRepo.find_by_id(UniqueId(self._bank_id))
